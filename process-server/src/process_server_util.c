ac41fb458e0e2cd6a5e3c814e9e50bbe0ccecb67
Maxwell Babey <babeymax@gmail.com>
2023-03-19 15:34:04 -0700

    Formatting

--- process-server/src/process_server_util.c
+++ process-server/src/process_server_util.c
@@ -100,38 +100,32 @@ int open_pipe_semaphores_domain_sockets(struct core_object *co, struct state_obj
 static int open_semaphores(struct core_object *co, struct state_object *so)
 {
     PRINT_STACK_TRACE(co->tracer);
+    
     sem_t *pipe_write_sem;
     sem_t *domain_read_sem;
     sem_t *domain_write_sem;
-    sem_t *log_sem;
     
     // Value 0 will block; value 1 will allow first process to enter, then behave as if value was 0.
     pipe_write_sem   = sem_open(PIPE_WRITE_SEM_NAME, O_CREAT, S_IRUSR | S_IWUSR, 1);
     domain_read_sem  = sem_open(DOMAIN_READ_SEM_NAME, O_CREAT, S_IRUSR | S_IWUSR, 0);
     domain_write_sem = sem_open(DOMAIN_WRITE_SEM_NAME, O_CREAT, S_IRUSR | S_IWUSR, 1);
-    log_sem          = sem_open(LOG_SEM_NAME, O_CREAT, S_IRUSR | S_IWUSR, 1);
-    if (pipe_write_sem == SEM_FAILED
-        || domain_read_sem == SEM_FAILED || domain_write_sem == SEM_FAILED
-        || log_sem == SEM_FAILED)
+    if (pipe_write_sem == SEM_FAILED || domain_read_sem == SEM_FAILED || domain_write_sem == SEM_FAILED)
     {
         SET_ERROR(co->err);
         // Closing an unopened semaphore will return -1 and set errno = EINVAL, which can be ignored.
         sem_close(pipe_write_sem);
         sem_close(domain_read_sem);
         sem_close(domain_write_sem);
-        sem_close(log_sem);
         // Unlinking an unopened semaphore will return -1 and set errno = ENOENT, which can be ignored.
         sem_unlink(PIPE_WRITE_SEM_NAME);
         sem_unlink(DOMAIN_READ_SEM_NAME);
         sem_unlink(DOMAIN_WRITE_SEM_NAME);
-        sem_unlink(LOG_SEM_NAME);
         return -1;
     }
     
     so->c_to_p_pipe_sem_write = pipe_write_sem;
     so->domain_sems[READ]  = domain_read_sem;
     so->domain_sems[WRITE] = domain_write_sem;
-    so->log_sem = log_sem;
     
     return 0;
 }
@@ -281,11 +275,9 @@ void p_destroy_parent_state(struct core_object *co, struct state_object *so, str
     sem_close(so->c_to_p_pipe_sem_write);
     sem_close(so->domain_sems[READ]);
     sem_close(so->domain_sems[WRITE]);
-    sem_close(so->log_sem);
     sem_unlink(PIPE_WRITE_SEM_NAME);
     sem_unlink(DOMAIN_READ_SEM_NAME);
     sem_unlink(DOMAIN_WRITE_SEM_NAME);
-    sem_unlink(LOG_SEM_NAME);
 }
 
 void c_destroy_child_state(struct core_object *co, struct state_object *so, struct child_struct *child)
