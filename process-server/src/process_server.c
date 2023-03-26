96881912dff0bacd617e746572850d128fbf6bf0
Maxwell Babey <babeymax@gmail.com>
2023-03-26 11:08:53 -0700

    Declare and define c_handle_http_request_response function

--- process-server/src/process_server.c
+++ process-server/src/process_server.c
@@ -169,6 +169,18 @@ static int c_get_file_description_from_domain_socket(struct core_object *co, str
                                                      struct child_struct *child);
 
 /**
+ * c_handle_http_request_response
+ * <p>
+ * Handle an http request and response.
+ * </p>
+ * @param co the core obejct
+ * @param so the state object
+ * @param child the child object
+ * @return 0 on success, -1 and set err on failure
+ */
+static int c_handle_http_request_response(struct core_object *co, struct state_object *so, struct child_struct *child);
+
+/**
  * c_inform_parent_recv_finished
  * <p>
  * Send the original fd number to the parent over the child-to-parent pipe.
@@ -534,9 +546,16 @@ static int c_receive_and_handle_messages(struct core_object *co, struct state_ob
             return -1;
         }
         
-        // TODO: c_handle_http_request_response(co, so, child);
-        (void) fprintf(stdout, "HTTP request handler not implemented.\n");
-    
+        if (errno == EINTR)
+        {
+            return 0;
+        }
+        
+        if (c_handle_http_request_response(co, so, child) == -1)
+        {
+            return -1;
+        }
+        
         if (c_inform_parent_recv_finished(co, so, child) == -1)
         {
             return -1;
@@ -544,6 +563,13 @@ static int c_receive_and_handle_messages(struct core_object *co, struct state_ob
         
         close_fd_report_undefined_error(child->client_fd_local, "state of child receive socket undefined.");
     }
+    
+    return 0;
+}
+
+static int c_handle_http_request_response(struct core_object *co, struct state_object *so, struct child_struct *child)
+{
+    PRINT_STACK_TRACE(co->tracer);
     
     return 0;
 }
