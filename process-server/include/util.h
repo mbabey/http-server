aa645c4d3014426ce65bdac283d6e449474e8185
Maxwell Babey <babeymax@gmail.com>
2023-03-26 20:51:38 -0700

    Add string append, set, and prepend to util.c/.h

--- process-server/include/util.h
+++ process-server/include/util.h
@@ -29,4 +29,52 @@ int write_fully(int fd, void * data, size_t size);
  */
 int read_fully(int fd, void * data, size_t size);
 
+/**
+ * set_string
+ * <p>
+ * Set a string to a value. If the string is NULL, will call malloc. If the string is not NULL, will call
+ * realloc.
+ * </p>
+ * <p>
+ * NOTE: Requires a dynamically allocated or NULL first parameter.
+ * </p>
+ * <p>
+ * <h3>
+ * WARNING: set_string dynamically allocates memory. Must free the pointer passed as the first parameter!
+ * </h3>
+ * </p>
+ * @param str - char**: pointer to the string to be set
+ * @param new_str - char*: the new value for the string
+ * @return the string, or NULL on failure
+ */
+char * set_string(char **str, const char *new_str);
+
+/**
+ * prepend_string
+ * <p>
+ * Prepend a prefix to a string.
+ * </p>
+ * <p>
+ * NOTE: Requires a dynamically allocated or NULL first parameter.
+ * </p>
+ * @param str - char**: pointer to the string to be modified
+ * @param prefix - char*: the string to be prepended
+ * @return the modified string, or NULL on failure
+ */
+char *prepend_string(char **str, const char *prefix);
+
+/**
+ * append_string
+ * <p>
+ * Append a suffix to a string.
+ * </p>
+ * <p>
+ * NOTE: Requires a dynamically allocated or NULL first parameter.
+ * </p>
+ * @param str - char**: pointer to the string to be modified
+ * @param suffix - char*: the string to be appended
+ * @return the modified string, or NULL on failure
+ */
+char *append_string(char **str, const char *suffix);
+
 #endif //POLL_SERVER_UTIL_H
--- process-server/src/util.c
+++ process-server/src/util.c
@@ -3,6 +3,7 @@
 #include <stdio.h>
 #include <string.h>
 #include <unistd.h>
+#include <stdlib.h>
 
 int write_fully(int fd, void * data, size_t size) {
     ssize_t result;
@@ -34,4 +35,67 @@ int read_fully(int fd, void * data, size_t size) {
     }
 
     return 0;
+}
+
+char * set_string(char **str, const char *new_str)
+{
+    size_t buf = strlen(new_str) + 1;
+    
+    if (!*str) // Double negative: true if the string is NULL
+    {
+        if((*str = (char *) malloc(buf)) == NULL)
+        {
+            return NULL;
+        }
+    } else
+    {
+        if ((*str = (char *) realloc(*str, buf)) == NULL)
+        {
+            return NULL;
+        }
+    }
+    
+    strcpy(*str, new_str);
+    
+    return *str;
+}
+
+char *prepend_string(char **str, const char *prefix)
+{
+    char *new_str;
+    size_t str_buf = strlen(*str) + 1;
+    size_t prefix_size = strlen(prefix);
+    
+    if ((new_str = (char *) calloc(str_buf + prefix_size, sizeof(char))) == NULL)
+    {
+        return NULL;
+    }
+    
+    strcat(new_str, prefix);
+    strcat(new_str, *str);
+    set_string(str, new_str);
+    
+    free(new_str);
+    
+    return *str;
+}
+
+char *append_string(char **str, const char *suffix)
+{
+    char *new_str;
+    size_t str_buf = strlen(*str) + 1;
+    size_t suffix_size = strlen(suffix);
+    
+    if ((new_str = (char *) calloc(str_buf + suffix_size, sizeof(char))) == NULL)
+    {
+        return NULL;
+    }
+    
+    strcat(new_str, *str);
+    strcat(new_str, suffix);
+    set_string(str, new_str);
+    
+    free(new_str);
+    
+    return *str;
 }
