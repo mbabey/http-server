51b3aadfeb1509db184f5d5e720709f087446810
Maxwell Babey <babeymax@gmail.com>
2023-03-27 11:21:09 -0700

    Add strcmp tree on request line method

--- process-server/src/methods.c
+++ process-server/src/methods.c
@@ -8,6 +8,25 @@ int perform_method(struct core_object *co, struct state_object *so, struct http_
                    size_t *status, struct http_header ***headers, char **entity_body)
 {
     // tree on request line
+    char *method;
+    
+    method = request->request_line->method;
+    
+    if (strcmp(method, "GET") == 0)
+    {
+        http_get();
+    } else if (strcmp(method, "HEAD") == 0)
+    {
+        http_head();
+    } else if (strcmp(method, "POST") == 0)
+    {
+        http_post();
+    } else
+    {
+        *status = NOT_IMPLEMENTED_501;
+        *headers = NULL;
+        *entity_body = NULL;
+    }
     
     // fill out status, headers, and entity body
     
